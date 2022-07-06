e8ccac9b523794c233954e841c06cec79b69c20e
westorres9 <wester.torres9@gmail.com>
2022-07-06 09:47:06 -0300

    requisiçao autorizada

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import qs from "qs";
 
 type LoginResponse = {  
@@ -38,6 +38,15 @@ export const requestBackendLogin = (loginData : LoginData ) => {
     });
 
     return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data: data, headers: headers });
+}
+
+export const requestBackend = (config: AxiosRequestConfig) => {
+
+    const headers = config.withCredentials ? {
+        ...config.headers,
+        Authorization: "Bearer " + getAuthData().access_token
+    } :config.headers;
+    return axios({...config, baseURL: BASE_URL, headers: headers});
 }
 
 export const saveAuthData = (obj: LoginResponse) => {
