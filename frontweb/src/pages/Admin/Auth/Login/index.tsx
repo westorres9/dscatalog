3454603a1eff445e51d64f87eb2b76fd8453cf70
westorres9 <wester.torres9@gmail.com>
2022-07-06 08:34:42 -0300

    Login Request

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
-import './styles.css';
 import { useForm } from 'react-hook-form';
+import { requestBackendLogin } from 'util/requests';
+import './styles.css';
 
 type FormData = {
   username: string;
@@ -13,7 +14,14 @@ const Login = () => {
   const { register, handleSubmit } = useForm<FormData>();
 
   const onSubmit = (formData : FormData) => {
-    console.log(formData);
+    requestBackendLogin(formData)
+    .then(response => {
+      console.log('SUCESSO', response);
+    })
+    .catch(error => {
+      console.log('ERRO', error);
+    })
+   
   };
 
   return (
