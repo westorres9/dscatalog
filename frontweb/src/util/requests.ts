fcb40443906aefa19f32a9b68ba2d2a5ddb9b17b
westorres9 <wester.torres9@gmail.com>
2022-07-07 10:21:51 -0300

    redirecionando para login quando autorizaçao falha

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 import qs from "qs";
+import history from './history';
 
 type LoginResponse = {  
     access_token: string;   
@@ -72,7 +73,9 @@ axios.interceptors.response.use(function (response) {
     console.log('INTERCEPTOR RESPOSTA COM SUCESOO');
     return response;
   }, function (error) {
-    console.log('INTERCEPTOR RESPOSTA COM ERRO');
+    if(error.response.status === 401  || error.response.status === 403) {
+        history.push('/admin/auth');
+    }
     return Promise.reject(error);
   });
 
